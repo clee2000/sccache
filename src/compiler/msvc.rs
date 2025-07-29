68df7c46ca4d6dce2c7c87401f17f53e3a11c959
Catherine Lee <csl@fb.com>
2025-07-29 10:32:51 -0700

    win 2022 build, add d2implyavx512upperregs flag

--- src/compiler/msvc.rs
+++ src/compiler/msvc.rs
@@ -432,6 +432,7 @@ msvc_args!(static ARGS: [ArgInfo<ArgData>; _] = [
     msvc_flag!("clr", PassThrough),
     msvc_take_arg!("clr:", OsString, Concatenated, PassThroughWithSuffix),
     msvc_take_arg!("constexpr:", OsString, Concatenated, PassThroughWithSuffix),
+    msvc_flag!("d2implyavx512upperregs-", PassThrough),
     msvc_take_arg!("deps", PathBuf, Concatenated, DepFile),
     msvc_take_arg!("diagnostics:", OsString, Concatenated, PassThroughWithSuffix),
     msvc_take_arg!("doc", PathBuf, Concatenated, TooHardPath), // Creates an .xdc file.
